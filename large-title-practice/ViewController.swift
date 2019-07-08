bb9ff6a5e1cc3524f37f20275a6d88f283e61c56
Shima <shima.jin@icloud.com>
2019-07-08 22:52:36 +0900

    Initial Commit

---
+++ large-title-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  large-title-practice
+//
+//  Created by jinsei_shima on 2019/07/08.
+//  Copyright © 2019 jinsei_shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
