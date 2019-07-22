f953dd46f5d85f26b2e633a6c948a28e148e5c5c
Shima <shima.jin@icloud.com>
2019-07-22 23:17:55 +0900

    update

--- large-title-practice/ViewController.swift
+++ large-title-practice/ViewController.swift
@@ -12,50 +12,100 @@ import EasyPeasy
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        self.navigationItem.largeTitleDisplayMode = .automatic
-        self.navigationController?.navigationBar.prefersLargeTitles = true
+        extendedLayoutIncludesOpaqueBars = true
+
+        navigationItem.largeTitleDisplayMode = .automatic
+        navigationController?.navigationBar.prefersLargeTitles = true
         
-//        let searchController = UISearchController.init(searchResultsController: nil)
-//        self.navigationItem.searchController = searchController
-//        self.navigationItem.searchController?.hidesNavigationBarDuringPresentation = false
+        navigationItem.title = "Main"
+        navigationItem.prompt = "Main Prompt"
         
-        self.navigationItem.title = "Main"
-        self.navigationItem.prompt = "Main Prompt"
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .bold),
+            NSAttributedString.Key.foregroundColor : UIColor.lightGray
+        ]
+        navigationController?.navigationBar.largeTitleTextAttributes =  [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .bold),
+            NSAttributedString.Key.foregroundColor : UIColor.darkText
+            ]
         
-//        self.navigationController?.navigationBar.titleTextAttributes
-//        self.navigationController?.navigationBar.largeTitleTextAttributes =  [
-//            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 20, weight: .bold),
-//            NSAttributedString.Key.foregroundColor : UIColor.darkText
-//            ]
-        
-        self.navigationController?.navigationBar.isTranslucent = false
-//        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = false
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.view.backgroundColor = .white
+
+        search_controller: do {
+            
+            let searchController = UISearchController.init(searchResultsController: nil)
+            //        searchController.searchResultsUpdater = self
+            //        searchController.obscuresBackgroundDuringPresentation = false
+            //        searchController.hidesNavigationBarDuringPresentation = false
+            
+            navigationItem.searchController = searchController
+            navigationItem.hidesSearchBarWhenScrolling = false
+            
+        }
         
         let rightBarButton = UIBarButtonItem.init(barButtonSystemItem: .add, target: nil, action: nil)
         let leftBarButton = UIBarButtonItem.init(barButtonSystemItem: .cancel, target: nil, action: nil)
 
-        self.navigationItem.setRightBarButton(rightBarButton, animated: false)
-        self.navigationItem.setLeftBarButton(leftBarButton, animated: false)
+        navigationItem.setRightBarButton(rightBarButton, animated: false)
+        navigationItem.setLeftBarButton(leftBarButton, animated: false)
+
+        custom_title_view: do {
+            
+//            let navigationView = UILabel()
+//            navigationView.backgroundColor = .orange
+//            navigationView.text = "custom view"
+//            navigationView.textColor = .lightText
+//            navigationItem.titleView = navigationView
+
+        }
 
         let scrollView = UIScrollView()
-        view.addSubview(scrollView)
         scrollView.backgroundColor = .groupTableViewBackground
         scrollView.contentSize = .init(width: view.bounds.width, height: view.bounds.height * 2)
-        scrollView.easy.layout(Edges())
+        view.addSubview(scrollView)
         
+        //        let refreshControl = UIRefreshControl()
+        //        refreshControl.addTarget(self, action: #selector(refresh(sender:)), for: .valueChanged)
+        //        scrollView.refreshControl = refreshControl
+
+        let box = UIView()
+        box.backgroundColor = .darkGray
+        scrollView.addSubview(box)
+
         let button = UIButton()
         button.setTitle("Detail", for: .normal)
         button.setTitleColor(.darkText, for: .normal)
         button.addTarget(self, action: #selector(didTap), for: .touchUpInside)
         view.addSubview(button)
-        button.easy.layout(Center())
+        
+        scrollView.easy.layout(
+            Edges()
+        )
+        
+        box.easy.layout(
+            Size(40),
+            Top(20),
+            CenterX()
+        )
+        
+        button.easy.layout(
+            Center()
+        )
         
     }
 
+    @objc func refresh(sender: UIRefreshControl) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            sender.endRefreshing()
+        }
+    }
+    
     @objc func didTap() {
         
         let detailVC = DetailViewController()
@@ -65,13 +115,14 @@ class ViewController: UIViewController {
 }
 
 class DetailViewController: UIViewController {
+    
     override func viewDidLoad() {
         
-        self.navigationItem.title = "Detail"
-        self.navigationItem.prompt = "Detail Prompt"
+        navigationItem.title = "Detail"
+        navigationItem.prompt = "Detail Prompt"
 
-//        self.navigationItem.largeTitleDisplayMode = .automatic
-//        self.navigationController?.navigationBar.prefersLargeTitles = true
+//        navigationItem.largeTitleDisplayMode = .automatic
+//        navigationController?.navigationBar.prefersLargeTitles = true
 
         super.viewDidLoad()
         view.backgroundColor = .white
